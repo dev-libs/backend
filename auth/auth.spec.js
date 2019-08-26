384758ae853a3554e2ac9e7975162b88e73386b4
Clark Williams <theclarkwilliams@gmail.com>
2019-08-26 15:53:21 -0600

    step 1 pg

---
+++ auth/auth.spec.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const authRouter = require('./auth-router.js');
+const db = require('../data/dbConfig.js');
+const Users = require('./auth-model.js');
+
+describe('register', () => {
+    beforeEach(async () => {
+        await db('users').truncate()
+    })
+    afterEach(async () => {
+        await db('users').truncate();
+    })
+    describe('register status', () => {
+        it('returns 201 created', () => {
+            return request(authRouter)
+                .post('/register')
+                .send({
+                    username: 'test',
+                    password: 'test'
+                })
+                .then(res => {
+                    expect(res.status).toBe(201);
+                })
+        })
+    })
+})
