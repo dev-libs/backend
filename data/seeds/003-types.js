ff32cfda18725ba441b39349b9fef6912215e085
Clark Williams <theclarkwilliams@gmail.com>
2019-08-27 14:40:24 -0600

    fixing heroku error

--- data/seeds/003-types.js
+++ data/seeds/003-types.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('types').truncate()
+  return knex('types')
     .then(function () {
       // Inserts seed entries
       return knex('types').insert([
