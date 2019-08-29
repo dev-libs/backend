e6a05e9d2a49944e39214fb2db8a238a82a6bd3c
Clark Williams <theclarkwilliams@gmail.com>
2019-08-29 11:05:29 -0600

    added .env file with JWT_SECRET

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -4,7 +4,8 @@ const secrets = require('./secrets');
 module.exports = (req, res, next) => {
     const token = req.headers.authorization;
     token
-        ? jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+        // ? jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+        ? jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
             err
                 ? res.status(401).json({ error: 'unauthorized' })
                 : req.user = { username: decodedToken.username }
